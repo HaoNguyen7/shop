21067837494f0a1fd67fec0f49e88e507b289b05
Hảo Nguyễn Văn <vanhao0643@gmail.com>
2021-07-10 10:25:53 +0700

    testing react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,35 @@
 
 import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './components/page/Home'
+import About from './components/page/About';
 function App() {
   return (
-    <div className="App">
-      <p>hello</p>
+    <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+      </Switch>
     </div>
+  </Router>
   );
 }
 
